e1ac4c6c5d5b8844d17c27c6f1658df73cf7d21f
Gurkan <hello@gurkz.me>
2026-03-25 12:27:59 +0100

    working version

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { run, command, string } from "@drizzle-team/brocli";
+import { getModSlugsFromFile } from "./lib/slugs.ts";
+import ProgressBar from "@deno-library/progress";
+import { ModrinthV2Client } from "@xmcl/modrinth";
+import { getProjectTitle, isOnVersion } from "./lib/project.ts";
 
 const check = command({
   name: "check",
@@ -6,9 +10,45 @@ const check = command({
   options: {
     file: string().required(),
     "mc-version": string().required(),
+    loader: string().required().enum("neoforge", "forge", "fabric"),
+    out: string().required(),
   },
   handler: async (options) => {
-    //
+    const modSlugs = await getModSlugsFromFile(options.file);
+
+    const bar = new ProgressBar({
+      title: "processing: ",
+      total: modSlugs.ids.length,
+    });
+
+    const client = new ModrinthV2Client();
+
+    const file = await Deno.open(options.out, {
+      write: true,
+      create: true,
+      truncate: true,
+    });
+
+    const lines: string[] = [];
+
+    for (const [index, id] of modSlugs.ids.entries()) {
+      await bar.render(index);
+
+      const title = await getProjectTitle(client, id);
+
+      const isIt = await isOnVersion(
+        client,
+        id,
+        options.loader,
+        options["mc-version"],
+      );
+
+      lines.push(`${title} - ${isIt ? "✅" : "❌"}`);
+    }
+
+    await file.write(new TextEncoder().encode(lines.join("\n") + "\n"));
+
+    file.close();
   },
 });
 
