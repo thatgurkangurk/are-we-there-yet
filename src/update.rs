8a5b37ab8c8df3bcfc2886c29a64bd0e5ff17830
Gurkan <hello@gurkz.me>
2026-06-30 17:24:35 +0200

    use msvc for the windows builds (#3)
    * add msvc to update.rs
    
    * build a msvc version
    
    * reusable updater logic

--- src/update.rs
+++ src/update.rs
@@ -4,21 +4,22 @@ use self_update::cargo_crate_version;
 pub fn update() -> Result<()> {
     let target = self_update::get_target();
 
-    let asset_name = match target {
-        "x86_64-pc-windows-gnu" => "are-we-there-yet-x86_64-pc-windows-gnu.exe",
-        "x86_64-unknown-linux-gnu" => "are-we-there-yet-x86_64-unknown-linux-gnu",
-        _ => panic!("Unsupported target {}", target),
+    let ext = if target.contains("windows") {
+        ".exe"
+    } else {
+        ""
     };
+
+    let asset_name = format!("are-we-there-yet-{}{}", target, ext);
 
     let status = self_update::backends::github::Update::configure()
         .repo_owner("thatgurkangurk")
         .repo_name("are-we-there-yet")
         .bin_name("are-we-there-yet")
-        .target(target)
-        .bin_name(asset_name)
+        .target(&target)
+        .bin_name(&asset_name)
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
-        .show_download_progress(true)
         .show_output(true)
         .build()?
         .update()?;
